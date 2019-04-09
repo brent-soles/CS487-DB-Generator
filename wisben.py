ae7b80c87900b4f29840956a09648674f3f00be8
mingjue <mingjue@vt.edu>
2019-04-08 18:12:32 -0700

    Try to get the cyclie string work

--- wisben.py
+++ wisben.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3.7
 
 import random
+import string
 
 class Incrementer:
     def __init__(self, low_value, high_value, increment_value):
@@ -35,6 +36,21 @@ class alpha():
         self.alpha = ''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUV') for i in range(1))
         return  self.alpha
 
+    def stringCyclicOne(self,start,end):
+        current_value = chr(start)+'xxxxxxxxxxxxxxxxxxxxxxxxx'+chr(start)+'xxxxxxxxxxxxxxxxxxxxxxxxx'+chr(start)
+        ret_val = current_value
+
+
+class CharIncrementer:
+    """ character incrementer """
+    def __init__(self,increaseValue):
+        """ constructor """
+        self.counter = Incrementer(65, 86, increaseValue)
+
+    def get_value(self):
+        """ return incremeter value"""
+        return chr(self.counter.get_value())
+
 def printOutTableOne():
     test = uniquleNumber()
     two = Incrementer(0, 1, 1)
@@ -49,9 +65,20 @@ def printOutTableOne():
     oddonehund = Incrementer(1,99,2)
     evenonehund = Incrementer(2,100,2)
     stringu1 = alpha()
-    
-    for i in range(0, 10):
-        un = test.outPut(100)
-        print(f"{un},{i},{two.get_value()},{four.get_value()},{ten.get_value()},{twenty.get_value()},{hundred.get_value()},{thousand.get_value()},{twothous.get_value()},{fivethous.get_value()},{tenthous.get_value()},{oddonehund.get_value()},{evenonehund.get_value()},{stringu1.random_alpha()}xxxxxxxxxxxxxxxxxxxxxxxxx{stringu1.random_alpha()}xxxxxxxxxxxxxxxxxxxxxxxxx{stringu1.random_alpha()}\n")
+
+    char_counter1 = CharIncrementer()
+    char_counter2 = CharIncrementer()
+    char_counter3 = CharIncrementer()
+    un = test.outPut(100)
+    for i in range(0, 2):
+        #print(f"{un},{i},{two.get_value()},{four.get_value()},{ten.get_value()},{twenty.get_value()},{hundred.get_value()},{thousand.get_value()},{twothous.get_value()},{fivethous.get_value()},{tenthous.get_value()},{oddonehund.get_value()},{evenonehund.get_value()},{stringu1.random_alpha()}xxxxxxxxxxxxxxxxxxxxxxxxx{stringu1.random_alpha()}xxxxxxxxxxxxxxxxxxxxxxxxx{stringu1.random_alpha()}\n")
         #print(f'{stringu1.random_alpha()}xxxxxxxxxxxxxxxxxxxxxxxxx{stringu1.random_alpha()}xxxxxxxxxxxxxxxxxxxxxxxxx{stringu1.random_alpha()}')
+        c_1 = char_counter1.get_value()
+        for j in range(0, 22):
+            c_2 = char_counter2.get_value()
+            for k in range(0, 22):
+                c_3 = char_counter3.get_value()
+                #print(f"{c_1}xx{c_2}xx{c_3}")
+    
 printOutTableOne()
+
